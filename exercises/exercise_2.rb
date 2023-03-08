5072afb1f43b9dcad00365c188a82d075c6c64d2
Samson Poon <samsonkfpoon@gmail.com>
2023-03-08 01:02:56 +0000

    add update delete store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.first
+puts @store1.name
+@store2 = Store.find_by(id: 2)
+
+@store1.update(name: "Brentwood")
+
+puts @store1.name
+puts @store2
